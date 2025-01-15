3aeb5ceb8cb482889d305286d60251da25d6d746
Bruno Bertolini <bruno.bertolini@mobile2you.com.br>
2025-01-15 15:10:03 -0300

    feat: add endless scroll base file

--- brunoccbertolini_base_android/src/main/java/com/brunoccbertolini/base/util/listners/EndlessRecyclerViewScrollListener.kt
+++ brunoccbertolini_base_android/src/main/java/com/brunoccbertolini/base/util/listners/EndlessRecyclerViewScrollListener.kt
@@ -1,58 +1,28 @@
-package br.com.nightking.base.ui.common
+package com.brunoccbertolini.base.util.listners
 
 import androidx.recyclerview.widget.RecyclerView
-import br.com.nightking.base.ui.base.RecyclerViewAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 abstract class EndlessRecyclerViewScrollListener : RecyclerView.OnScrollListener {
-    // The minimum amount of items to have below your current scroll position
-    // before loading more.
+
     private var visibleThreshold = 5
 
-    // The current offset index of data you have loaded
-    var currentPage = 0
+    var currentPage = 1
         private set
 
-    // The total number of items in the dataset after the last load
     private var previousTotalItemCount = 0
 
-    // True if we are still waiting for the last set of data to load.
     private var loading = true
 
-    // Sets the starting page index
-    private val startingPageIndex = 0
-    var mLayoutManager: RecyclerView.LayoutManager
-    var mAdapter: RecyclerViewAdapter? = null
-    fun setAdapter(adapter: RecyclerViewAdapter?) {
+    private val startingPageIndex = 1
+    private var mLayoutManager: LinearLayoutManager
+    private var mAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>? = null
+    fun setAdapter(adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>) {
         mAdapter = adapter
     }
 
     constructor(layoutManager: LinearLayoutManager) {
         mLayoutManager = layoutManager
-    }
-
-    constructor(layoutManager: GridLayoutManager) {
-        mLayoutManager = layoutManager
-        visibleThreshold *= layoutManager.spanCount
-    }
-
-    constructor(layoutManager: StaggeredGridLayoutManager) {
-        mLayoutManager = layoutManager
-        visibleThreshold *= layoutManager.spanCount
-    }
-
-    private fun getLastVisibleItem(lastVisibleItemPositions: IntArray): Int {
-        var maxSize = 0
-        for (i in lastVisibleItemPositions.indices) {
-            if (i == 0) {
-                maxSize = lastVisibleItemPositions[i]
-            } else if (lastVisibleItemPositions[i] > maxSize) {
-                maxSize = lastVisibleItemPositions[i]
-            }
-        }
-        return maxSize
     }
 
     /**
@@ -62,25 +32,11 @@ abstract class EndlessRecyclerViewScrollListener : RecyclerView.OnScrollListener
     */
     override fun onScrolled(view: RecyclerView, dx: Int, dy: Int) {
         var lastVisibleItemPosition = 0
-        val totalItemCount = mAdapter!!.displayableItemsCount
+        val totalItemCount = mAdapter!!.itemCount
 
         if (dy == 0) return
 
-        when (mLayoutManager) {
-            is StaggeredGridLayoutManager -> {
-                val lastVisibleItemPositions =
-                    (mLayoutManager as StaggeredGridLayoutManager).findLastVisibleItemPositions(null)
-                lastVisibleItemPosition = getLastVisibleItem(lastVisibleItemPositions)
-            }
-            is GridLayoutManager -> {
-                lastVisibleItemPosition =
-                    (mLayoutManager as GridLayoutManager).findLastVisibleItemPosition()
-            }
-            is LinearLayoutManager -> {
-                lastVisibleItemPosition =
-                    (mLayoutManager as LinearLayoutManager).findLastVisibleItemPosition()
-            }
-        }
+        lastVisibleItemPosition = mLayoutManager.findLastVisibleItemPosition()
 
 
         if (isTotalItemCountZero(totalItemCount)) {
@@ -123,14 +79,6 @@ abstract class EndlessRecyclerViewScrollListener : RecyclerView.OnScrollListener
         }
     }
 
-    private fun isTotalItemCountZero(totalItemCount: Int) =
-        totalItemCount < previousTotalItemCount
-
-    fun resetState() {
-        currentPage = startingPageIndex
-        previousTotalItemCount = 0
-        loading = true
-    }
-
+    private fun isTotalItemCountZero(totalItemCount: Int) = totalItemCount < previousTotalItemCount
     abstract fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView?)
 }
